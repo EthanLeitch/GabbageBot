5e4b5dfd8ec766a20849d7c691692a9243a32126
EthanLeitch <ethanjohnleitch@gmail.com>
2020-04-20 23:20:55 +0000

    added warning to ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,5 @@
 var emoji = require('node-emoji')
 module.exports = (client) => {
   console.log(emoji.get('beer') + ` Ready to serve ${client.guilds.size} servers, for a total of ${client.users.size} users!`);
+  console.log(`WARNING: If you stop this program, the bot will go offline and you will need to restart it with node index.js`)
 }
